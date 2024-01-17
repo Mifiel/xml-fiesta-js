acab4adee7ea9ba8ae73d59a0722c681fb4788c7
Jesús Alejandro López Loría <alejandro@mifiel.com>
2024-01-17 16:20:36 -0600

    feat: create transfer class with validations #865cu6cyj

---
+++ src/transfer.ts
@@ -0,0 +1,112 @@
+import Certificate from "./certificate";
+import { b64toHex } from "./common";
+import Document from "./document";
+import { GetTransfersByTxIdResult } from "./services/blockchain/liquid";
+import Signature from "./signature";
+
+export default class Transfer extends Document {
+  prevAddress: string;
+  currentAddress: string;
+  dataBlockchain: GetTransfersByTxIdResult;
+
+  constructor(file, options, transferData) {
+    super(file, options);
+
+    this.prevAddress = transferData.prevAddress;
+    this.currentAddress = transferData.currentAddress;
+    this.dataBlockchain = transferData.dataBlockchain;
+  }
+
+  validEndorser(rootCertificates) {
+    const isValidHolder = this.validateHolderBinding(
+      this.prevHolder,
+      rootCertificates
+    );
+
+    const nameIsValid = this.validateName(this.prevHolder);
+
+    if (!isValidHolder || !nameIsValid)
+      return {
+        isValid: false,
+        error_code: "integrity",
+      };
+
+    const isConsistentWithBlockchain =
+      this.prevAddress === this.dataBlockchain.prevAddress;
+
+    if (!isConsistentWithBlockchain) return {
+      isValid: false,
+      error_code: "inconsistent_with_blockchain",
+    };
+
+    return {
+      isValid: true,
+    };
+  }
+
+  validEndorsee(rootCertificates) {
+    const isValidHolder = this.validateHolderBinding(this.currentHolder, rootCertificates);
+
+    const nameIsValid = this.validateName(this.currentHolder)
+
+    if (!isValidHolder || !nameIsValid)
+      return {
+        isValid: false,
+        error_code: "integrity",
+      };
+
+    const isConsistentWithBlockchain =
+      this.currentAddress === this.dataBlockchain.currentAddress;
+
+    if (!isConsistentWithBlockchain) return {
+      isValid: false,
+      error_code: "inconsistent_with_blockchain",
+    };
+
+    return {
+      isValid: true
+    };
+  }
+
+  validateName(holder) {
+    const nameHolderNode = holder.$.name;
+    const nameHolderPlaintext =
+      holder.binding[0].signature[0].$.plaintext
+        .split("|")
+        .slice(-1)[0];
+
+    return nameHolderNode === nameHolderPlaintext;
+
+  }
+
+  validateHolderBinding(holder, rootCertificates) {
+    // validate certificate
+    // the signature corresponds to one of the certificates
+    const certificate = new Certificate(
+      null,
+      b64toHex(holder.binding[0].certificate[0]._)
+    );
+    const isCa = rootCertificates.some((rootCer) =>
+      certificate.isCa(rootCer.cer_hex)
+    );
+
+    // validate signature
+    const nodeSignature = holder.binding[0].signature[0];
+    const signatureData = {
+      signedAt: nodeSignature.$.signedAt,
+      signatureHex: b64toHex(nodeSignature._),
+    };
+
+    const signatureInstance = new Signature(
+      certificate.hex,
+      signatureData.signatureHex,
+      signatureData.signedAt,
+      null,
+      null
+    );
+
+    const isValidSignature = signatureInstance.valid(nodeSignature.$.plaintext);
+
+    return isCa && isValidSignature
+  }
+}
